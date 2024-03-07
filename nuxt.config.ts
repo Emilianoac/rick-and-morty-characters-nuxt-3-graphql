5126244818823ca66973ea390134bbcd7b19a990
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2024-03-06 23:06:03 -0300

    ADD: Site title.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Rick and Morty Characters",
+    },
+  },
   devtools: { enabled: true },
   modules: [
     "nuxt-graphql-client",
