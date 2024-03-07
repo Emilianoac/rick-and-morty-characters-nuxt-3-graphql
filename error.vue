5e84b3031993fd6e0fd842a61bcbe30549e8bbb7
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2024-03-06 23:05:31 -0300

    ADD: Error Page..

---
+++ error.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+  const props = defineProps( {
+    error: Object as any
+  })
+
+  useSeoMeta({
+    title: `${props.error.statusCode} | Rick and Morty Characters`,
+    description: 'Error',
+    keywords: 'Error'
+  })
+</script>
+
+<template>
+  <Navbar/>
+  <UContainer>
+    <div class="text-center mt-20">
+      <img class="max-w-48 mx-auto" src="/error.png" alt="Error"/>
+      <h1 class="text-8xl font-bold mx-auto mt-5">{{ error.statusCode }}</h1>
+      <p class="mt-3">{{ error.message }}</p>
+    </div>
+  </Ucontainer>
+</template>
+
+<style lang="scss" scoped>
+  
+</style>
